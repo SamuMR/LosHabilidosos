584b95ebadb2f46537dd7a61ccfc157cdab6f462
TavoTksh1 <143555568+TavoTksh1@users.noreply.github.com>
2023-09-16 16:18:07 -0500

    Metas v3
    Actualiza los textos ingresados

--- Project-Web/Project Web/Prueba/Metas/script_goals.js
+++ Project-Web/Project Web/Prueba/Metas/script_goals.js
@@ -1,7 +1,17 @@
 const btn_add = document.getElementById("btn_add");
 const txt_field = document.getElementById("txt_field")
+const txt_added = document.getElementById("txt_added")
 
 btn_add.addEventListener("click", function(){
     txt_field.style.display = "block";
-
 });
+
+txt_field.addEventListener("keyup", function(event){
+    if (event.key == "Enter"){
+        const texto = txt_field.value;
+        txt_added.textContent =  `Texto ingresado: ${texto}`;
+    
+        txt_field.value="";
+        txt_field.style.display= "none";
+    }
+})
