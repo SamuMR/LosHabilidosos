4e837236d40f94a3429b23fb5984ac4712b282ad
TavoTksh1 <143555568+TavoTksh1@users.noreply.github.com>
2023-09-16 20:25:21 -0500

    Metas v2
    Acción agregar

---
+++ Project-Web/Project Web/Prueba/Metas/script_goals.js
@@ -0,0 +1,7 @@
+const btn_add = document.getElementById("btn_add");
+const txt_field = document.getElementById("txt_field")
+
+btn_add.addEventListener("click", function(){
+    txt_field.style.display = "block";
+
+});
