a472db390f4c95ace9c8ffb75db4ef238661b660
TavoTksh1 <143555568+TavoTksh1@users.noreply.github.com>
2023-09-16 23:00:36 -0500

    Metas v5
    Verificacion de campos de texto

--- Project-Web/Project Web/Prueba/Metas/script_goals.js
+++ Project-Web/Project Web/Prueba/Metas/script_goals.js
@@ -1,15 +1,13 @@
 const btn_add = document.getElementById("btn_add");
 const txt_field = document.getElementById("txt_field");
 const txt_container = document.getElementById("txt_container");
+const word_count = document.getElementById("word-count");
 const textosIngresados = [];
 
 btn_add.addEventListener("click", function () {
-  // Obtén el valor del placeholder desde el atributo data-placeholder
   const placeholderValue = txt_field.getAttribute("data-placeholder");
-
-  // Asigna el valor del placeholder al campo de texto
   txt_field.value = placeholderValue;
-
+  word_count.style.display = "block";
   txt_field.style.display = "block";
   txt_field.focus();
 });
@@ -26,38 +24,31 @@ function crearTextoConIconos(texto) {
   textoElement.textContent = texto;
   nuevoDiv.appendChild(textoElement);
 
+  // Editar //
   const iconoEditar = document.createElement("img");
   iconoEditar.src = "resources/edit.png";
   iconoEditar.alt = "Editar";
   iconoEditar.addEventListener("click", function () {
-    // Obtén el valor del placeholder desde el atributo data-placeholder
     const placeholderValue = textoElement.getAttribute("data-placeholder");
-
-    // Asigna el valor del placeholder al campo de texto
     txt_field.value = placeholderValue;
-
-    // Habilita la edición del texto
     textoElement.contentEditable = "true";
     textoElement.focus();
 
-    // Captura la modificación al presionar "Enter" y previene el salto de línea
     textoElement.addEventListener("keydown", function (event) {
       if (event.key === "Enter") {
-        event.preventDefault(); // Evita el salto de línea
+        event.preventDefault();
         textoElement.contentEditable = "false";
       }
     });
-
-    // Limpia el atributo data-placeholder
     textoElement.removeAttribute("data-placeholder");
   });
   divIcon.appendChild(iconoEditar);
 
+  // Borrar //
   const iconoBorrar = document.createElement("img");
   iconoBorrar.src = "resources/trash-bin.png";
   iconoBorrar.alt = "Borrar";
   iconoBorrar.addEventListener("click", function () {
-    // Eliminar el contenedor del texto
     nuevoDiv.remove();
   });
   divIcon.appendChild(iconoBorrar);
@@ -65,16 +56,4 @@ function crearTextoConIconos(texto) {
   return nuevoDiv;
 }
 
-txt_field.addEventListener("keyup", function (event) {
-  if (event.key === "Enter") {
-    const texto = txt_field.value;
-    textosIngresados.push(texto);
 
-    const nuevoDiv = crearTextoConIconos(texto);
-
-    txt_container.appendChild(nuevoDiv);
-
-    txt_field.value = "";
-    txt_field.style.display = "none";
-  }
-});
