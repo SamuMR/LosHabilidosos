65800d155f8e5f676035d7421cd2e2042cbebc5b
TavoTksh1 <143555568+TavoTksh1@users.noreply.github.com>
2023-11-04 14:05:47 -0500

    Cargado a Spring

---
+++ Project-Web/Project Web/h2habil/src/main/java/los/habilidosos/h2habil/H2habilApplication.java
@@ -0,0 +1,13 @@
+package los.habilidosos.h2habil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2habilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2habilApplication.class, args);
+	}
+
+}
---
+++ Project-Web/Project Web/h2habil/src/test/java/los/habilidosos/h2habil/H2habilApplicationTests.java
@@ -0,0 +1,13 @@
+package los.habilidosos.h2habil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2habilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
