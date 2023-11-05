dea94630526301162764eaf4db718d9d8ec57fa6
TavoTksh1 <143555568+TavoTksh1@users.noreply.github.com>
2023-11-05 17:53:56 -0500

    Revert "Inicios de los cursos"
    This reverts commit f8f37b39e50f988739bca795a4bb689a7a6658bd.

---
+++ habil2/target/classes/static/js/apps/signinForm.js
@@ -0,0 +1,29 @@
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js"
+import { auth } from "./firebase.js"
+import { showMessage } from "./showMessage.js"
+const signInForm = document.querySelector("#login-form");
+
+signInForm.addEventListener("submit",async e=>{
+
+    e.preventDefault
+    const email = signInForm["login-email"].value
+    const password = signInForm["login-password"].value
+
+    try {
+        const credentials = await signInWithEmailAndPassword(auth, email, password)
+        console.log(credentials)
+
+    const modal = bootstrap.Modal.getInstance(document.querySelector("#singinModal"))
+    modal.hide()
+
+    showMessage("Bienvenido " + credentials.user.email)
+    } catch (error) {
+        console.log(error)
+        if(error.code === "auth/invalid-login-credentials"){
+            showMessage("credenciales equivocadas" , "error")
+        }else if(error.code === "auth/too-many-requests"){
+
+            showMessage("demasiados intentos erroneos", "error")
+        }
+    }
+})
