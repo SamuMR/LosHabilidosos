16e5c18aecbc37801e045bc2ee8ad5bd7a7d1d63
TavoTksh1 <143555568+TavoTksh1@users.noreply.github.com>
2023-11-30 19:54:56 -0500

    a2

--- Project-Web/Project Web/habil2/src/main/java/los/habilidosisimos/habil2/controller/IndexController.java
+++ Project-Web/Project Web/habil2/src/main/java/los/habilidosisimos/habil2/controller/IndexController.java
@@ -1,15 +1,13 @@
 package los.habilidosisimos.habil2.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class IndexController {
-
-    @Autowired
-    private SesionController sesionController;
 
     @GetMapping
     public String index(){
--- Project-Web/Project Web/habil2/src/main/java/los/habilidosisimos/habil2/repository/SesionRepository.java
+++ Project-Web/Project Web/habil2/src/main/java/los/habilidosisimos/habil2/repository/SesionRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import los.habilidosisimos.habil2.entity.Sesion;
 
 public interface SesionRepository extends JpaRepository<Sesion, Long>{
-    Sesion findByUsuarioActual(String usuarioActual);
+
 }
